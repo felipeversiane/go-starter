5725caecaee568cf8776594a3a071073fbf0cb6e
felipeversiane <felipeversiane09@gmail.com>
2024-12-08 10:14:03 -0300

    feat: add domain structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+
+	"github.com/felipeversiane/go-starter/internal/infra/config"
+	"github.com/felipeversiane/go-starter/internal/infra/config/log"
+	"github.com/felipeversiane/go-starter/internal/infra/database"
+	"github.com/felipeversiane/go-starter/internal/infra/server"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	cfg := config.NewConfig()
+
+	logger := log.NewLogger(cfg.GetLogConfig())
+	logger.Configure()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	database := database.NewDatabaseConnection(ctx, cfg.GetDatabaseConfig())
+	defer database.Close()
+
+	server := server.NewServer(cfg.GetServerConfig(), database)
+	server.SetupRouter()
+	server.Start()
 }

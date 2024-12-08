1af0edff03c22f2f6f9a2fd3b154ea4c628a449b
felipeversiane <felipeversiane09@gmail.com>
2024-12-08 09:54:11 -0300

    feat: add server app

---
+++ internal/infra/server/server.go
@@ -0,0 +1,55 @@
+package server
+
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/felipeversiane/go-starter/internal/infra/config"
+	"github.com/felipeversiane/go-starter/internal/infra/config/log"
+	"github.com/felipeversiane/go-starter/internal/infra/services/database"
+	"github.com/gin-gonic/gin"
+)
+
+type ServerInterface interface {
+	SetupRouter()
+	Start()
+}
+
+type server struct {
+	router   *gin.Engine
+	config   config.ServerConfig
+	database database.DatabaseInterface
+}
+
+func NewServer(
+	cfg config.ServerConfig,
+	database database.DatabaseInterface,
+) ServerInterface {
+	server := &server{
+		router:   gin.New(),
+		config:   cfg,
+		database: database,
+	}
+	return server
+}
+
+func (s *server) SetupRouter() {
+	s.router.Use(gin.Recovery())
+	s.router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "healthy",
+		})
+	})
+}
+
+func (s *server) Start() {
+	s.router.Use(log.LogMiddleware())
+	port := s.config.Port
+	if port == "" {
+		port = "8000"
+	}
+	err := s.router.Run(fmt.Sprintf(":%s", port))
+	if err != nil {
+		slog.Error("failed to start server", slog.Any("error", err))
+	}
+}
